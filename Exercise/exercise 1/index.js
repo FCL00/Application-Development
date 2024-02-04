fa35f475839d2145a09779c222b90f0f65109307
Fernando Lagahit <flagahit00@gmail.com>
2024-02-04 16:35:18 +0800

    Exercises

---
+++ Exercise/exercise 1/index.js
@@ -0,0 +1,18 @@
+// Seatwork: Grade Checker
+
+// Create a program that checks a student's grade based on their exam score.
+// Use if-else statements to determine the grade.
+// The grading scale is as follows:
+// - A: 90-100
+// - B: 80-89
+// - C: 70-79
+// - D: 60-69
+// - F: Below 60
+
+let examScore = 0; // change the value
+
+if (examScore) {
+  // your code goes here
+} else {
+  // your code goes here
+}
---
+++ Exercise/exercise 2/index.js
@@ -0,0 +1,8 @@
+// ==============================================================================//
+// Seatwork: Countdown
+// Implement a countdown using a while loop.
+// Start the countdown from 10 and print the values down to 1.
+
+// ==============================================================================//
+// Seatwork: Sum of Even Numbers
+// Use a for loop to calculate and print the sum of even numbers from 2 to 10.
