61982d77e29b53bf8ace792721c5a0ecc936bab4
Fernando Lagahit <flagahit00@gmail.com>
2024-02-05 00:00:26 +0800

    ES6 Modules

---
+++ JAVASCRIPT/6.2 ES6 Modules/public/assets/js/main.js
@@ -0,0 +1,15 @@
+export const API_KEY = "asgfdhasdgahfdas";
+
+export function onClickHandler() {
+  console.log("Hey you click me");
+}
+
+function Hello() {
+  console.log("Message");
+}
+
+function H1() {
+  console.log("Message");
+}
+
+export { Hello, H1 };
